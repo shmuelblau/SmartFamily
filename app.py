88c04ef6ae621c36cc5772d6875e3117db68d5d7
shmuelblau <a0528075188@gmail.com>
2024-06-27 16:36:37 +0300

    Starting a new project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request,jsonify, url_for , redirect,session
+from ExampleSql import familys, passwords
+app=Flask(__name__)
+
+@app.route('/')
+def start():
+    return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+    
+    return render_template('home.html')
+    
+if __name__ == ('__main__'):
+    app.run(debug=True)
+
